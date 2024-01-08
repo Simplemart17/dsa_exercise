d6e90a1b247723cba105f8ea0ffd6046a971a9e1
Martins Aloba <aloba.martins@gmail.com>
2024-01-07 20:43:39 -0500

    add two sum function

--- TwoSum/index.js
+++ TwoSum/index.js
@@ -20,7 +20,6 @@ const twoSumA = (num, target) => {
     }
   }
 
-
   // when no number in the array add up to the target
   return [];
 }
@@ -39,10 +38,9 @@ const twoSumB = (num, target) => {
     const complementaryNum = target - currentNumber;
 
     // check if the object contains complementary number
-    if (hash[complementaryNum] !== undefined) {
-
-  
+    if (hash[complementaryNum] !== undefined) {  
       // get the index of the seen number and that of the complementary number
+
       return [hash[complementaryNum], i];
     }
 
@@ -54,52 +52,5 @@ const twoSumB = (num, target) => {
   return [];
 }
 
-// Given an array num, define a running sum of an array as runningSum[i] = sum(num[0]...num[i]);
-// Constraints: 
-// - The number of nodes in the list is in the range [1, 1000]
-// - The value of each element in the array is in the range [-10^6, 10^6]
-// Input: num = [3, 1, 2, 10, 1]
-// Output: [3, 4, 6, 16, 17]
-const runningSum = (num) => {
-  // let result = [];
-  // let ans = num[0];
-  // for (let i =0; i < num.length; i++) {
-  //   result.push(ans);
-  //   ans = ans + num[i + 1]
-  // }
-  // return result;
- 
-  // overriding the original array
-  for (let i = 1; i < num.length; i++) {
-    num[i] = num[i - 1] + num[i];
-  }
-
-  return num;
-}
-
-// // process logs
-// function processLogs(logs, threshold) {
-//   // Write your code here.
-//   const hash = {};
-//   const result = [];
-//   for (let i = 0; i < logs.length; i++) {
-//     const [ip, time] = logs[i].split(' ');
-//     if (hash[ip] === undefined) {
-//       hash[ip] = time;
-//     } else {
-//       hash[ip] = Math.max(hash[ip], time);
-//     }
-//   }
-
-//   for (let key in hash) {
-//     if (hash[key] - threshold > 0) {
-//       result.push(key);
-//     }
-//   }
-
-//   return result;
-// }
-
-// console.log(twoSumA([2,7,11,15], 13));
-// console.log(twoSumB([2,7,11,15], 13));
-console.log(runningSum([3, 1, 2, 10, 1]));
+console.log(twoSumA([2,7,11,15], 26));
+console.log(twoSumB([2,7,11,15], 26));

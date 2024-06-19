7e051350ad7326fae98310840c560cb6124f2fcc
Martins Aloba <aloba.martins@gmail.com>
2024-06-18 22:01:07 -0400

    add python implementation of two_sum

--- Array/two_sum.py
+++ Array/two_sum.py
@@ -8,3 +8,18 @@
  Output: [0,1]
  Explanation: Because num[0] + num[1] == 9, we return [0, 1].
 """
+from typing import List
+
+class Solution:
+    def twoSum(self, num: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(num):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+        return []
+
+
+s = Solution()
+print(s.twoSum([2,7,11,15], 26))
